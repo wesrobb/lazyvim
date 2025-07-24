27c7586731f4ab4a06b60e3cf1acd03db1b2d639
Wesley Robb <wesley.robb@derivco.co.za>
2025-07-24 16:29:22 +0200

    Show diagnostic source

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -4,7 +4,14 @@ return {
     event = "LspAttach", -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
     config = function()
-      require("tiny-inline-diagnostic").setup()
+      require("tiny-inline-diagnostic").setup({
+        options = {
+          show_source = {
+            enabled = true,
+            if_many = true,
+          },
+        },
+      })
       vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
   },
